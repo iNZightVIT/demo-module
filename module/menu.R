aafbc3b8dfc93dd38722fc866d97cd6134e21aaf
Tom Elliott <tomelliottnz@gmail.com>
2022-03-25 12:32:04 +1300

    alternative custom add options

--- module/menu.R
+++ module/menu.R
@@ -2,13 +2,14 @@ menu <- list(
     Variables = list(
         Numeric = item(
             title = "Demo numeric variables",
-            action = function(gui) DemoNumVar$new(gui)
+            action = function(gui, mod) DemoNumVar$new(gui, mod)
         )
     ),
+    # Modules = item("Demo Module Here", function(gui, mod) DemoModule$new(gui, mod = mod))
     Modules = list(
         Demo = list(
-            item("Demo 1", function(gui) DemoModule$new(gui, which = 1L)),
-            item("Demo 1", function(gui) DemoModule$new(gui, which = 1L))
+            item("Demo 1", function(gui, mod) DemoModule$new(gui, mod, which = 1L)),
+            item("Demo 2", function(gui, mod) DemoModule$new(gui, mod, which = 2L))
         )
     )
 )
