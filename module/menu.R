75f59f5b343337b039e6cdf40ff0600e717a00c6
Tom Elliott <tomelliottnz@gmail.com>
2024-01-18 08:15:24 +1300

    re-enable demo menu

--- module/menu.R
+++ module/menu.R
@@ -1,17 +1,17 @@
-# menu <- list(
-#     Variables = list(
-#         `Numeric Variables` = list(
-#             demo = item(
-#                 title = "Demo numeric variables",
-#                 action = function(gui, mod) DemoNumVar$new(gui, mod)
-#             )
-#         )
-#     ),
-#     # Modules = item("Demo Module Here", function(gui, mod) DemoModule$new(gui, mod = mod))
-#     Modules = list(
-#         Demo = list(
-#             item("Demo 1", function(gui, mod) DemoModule$new(gui, mod, which = 1L)),
-#             item("Demo 2", function(gui, mod) DemoModule$new(gui, mod, which = 2L))
-#         )
-#     )
-# )
+menu <- list(
+    Variables = list(
+        `Numeric Variables` = list(
+            demo = item(
+                title = "Demo numeric variables",
+                action = function(gui, mod) DemoNumVar$new(gui, mod)
+            )
+        )
+    ),
+    # Modules = item("Demo Module Here", function(gui, mod) DemoModule$new(gui, mod = mod))
+    Modules = list(
+        `Demo Module` = list(
+            item("Demo 1", function(gui, mod) DemoModule$new(gui, mod, which = 1L)),
+            item("Demo 2", function(gui, mod) DemoModule$new(gui, mod, which = 2L))
+        )
+    )
+)
